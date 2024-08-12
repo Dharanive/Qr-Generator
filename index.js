e462059516842b5d2612c7d192dfae6714ed2583
Dharanivel <90686595+Dharanive@users.noreply.github.com>
2024-08-12 09:14:22 +0530

    Update index.js
    Line with qr.image(url, { type: 'png' });: This line now correctly uses url, the variable that holds the user's input, instead of the hardcoded string 'url!'.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.URL;
-    var qr_svg = qr.image('url!', { type: 'png' });
+    var qr_svg = qr.image(url, { type: 'png' });
     qr_svg.pipe(fs.createWriteStream('qr-img.png'));
 
     fs.writeFile('URL.txt', url, (err) => {
